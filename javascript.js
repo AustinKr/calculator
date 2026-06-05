3b2239d3eee12b05b98bc584f58d46c837198124
Austin K <40644715+AustinKr@users.noreply.github.com>
2026-06-05 13:57:19 -0700

    Add number display

--- javascript.js
+++ javascript.js
@@ -17,6 +17,14 @@ function multiply(a, b)
 
 function main()
 {
-    
+    const displayResult = document.querySelector(".display-result");
+    const displayCurrent = document.querySelector(".display-current");
+
+    const panelNumpad = document.querySelector(".panel-numpad");
+    panelNumpad.addEventListener("click", event => {
+        let index = Array.from(panelNumpad.children).indexOf(event.target);
+        if(index >= 0)
+            displayCurrent.textContent = index;
+    });
 }
 main();
