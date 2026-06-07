54faa256eb4ced5c437157dfeb841e39edf47606
austin <40644715+AustinKr@users.noreply.github.com>
2026-06-06 22:35:30 -0700

    Separate code into reusable functions

--- javascript.js
+++ javascript.js
@@ -34,7 +34,7 @@ function containsDecimalPoint(float) {
     return (float - Math.floor(float)) !== 0;
 }
 
-let entries = [null, null, null];
+let entries = [null, null, null]; // TODO: make entries A and B store string instead of floats
 let shouldAddDecimalPoint = false;
 
 function setEntry(index, digit)
@@ -43,7 +43,7 @@ function setEntry(index, digit)
     {
         let digitStr = digit.toString();
         if (shouldAddDecimalPoint && !containsDecimalPoint(entries[index]))
-            digitStr = (digit / 10.0).toString().substr(1); // add decimal point, and omit 0 from '0.[digit]'  
+            digitStr = ".".concat(digitStr);
         shouldAddDecimalPoint = false;
 
         entries[index] = appendDigitToFloat(entries[index], digitStr);
@@ -85,10 +85,69 @@ function getEntriesDisplayText()
     return `${a} ${sign} ${b}`;
 }
 
+const displayResult = document.querySelector(".display-result");
+const displayCurrent = document.querySelector(".display-current");
+
+function enterDigit(digit)
+{
+    let currentID = getCurrentEntryID();
+    if (currentID <= 1)
+        setEntry(0, digit);
+    else if (currentID > 1)
+        setEntry(2, digit);
+
+    displayCurrent.textContent = getEntriesDisplayText();
+}
+function deleteDigit()
+{
+    let id = getCurrentEntryID() - 1;
+    switch (id) {
+        case -1: // No entries in current line
+            displayResult.textContent = "";
+        case 0: // A has a value
+        case 2: // B has a value
+            setEntry(id, null);
+            break;
+
+        default: // Operator has a value
+            entries[id] = null;
+    }
+    displayCurrent.textContent = getEntriesDisplayText();
+}
+function enterOperator(operatorID)
+{
+    let currentID = getCurrentEntryID();
+    if (currentID < 1) {
+        displayResult.textContent = "enter a number";
+        return;
+    }
+    entries[1] = operatorID;
+    displayCurrent.textContent = getEntriesDisplayText();
+}
+function enterEvaluate()
+{
+    if (getCurrentEntryID() < 2) {
+        displayResult.textContent = "enter a expression";
+        return;
+    }
+
+    let result = evaluate(entries[0], entries[2], entries[1]);
+    displayResult.textContent = result;
+    displayCurrent.textContent = "";
+    entries = [result, null, null];
+}
+
 function main()
 {
-    const displayResult = document.querySelector(".display-result");
-    const displayCurrent = document.querySelector(".display-current");
+    // const container = document.querySelector(".container");
+    // container.addEventListener("keyup", event =>
+    // {
+    //     switch (event.key)
+    //     {
+    //         case 0:
+
+    //     }
+    // });
 
     const panelNumpad = document.querySelector(".panel-numpad");
     panelNumpad.addEventListener("click", event =>
@@ -96,64 +155,20 @@ function main()
         let index = Array.from(panelNumpad.children).indexOf(event.target);
         if(index < 0)
             return;
-
-        let currentID = getCurrentEntryID();
-        if (currentID <= 1)
-            setEntry(0, index);
-        else if(currentID > 1)
-            setEntry(2, index);
-
-        displayCurrent.textContent = getEntriesDisplayText();
+        enterDigit(index);
     });
-    document.getElementById("delete-button").addEventListener("click", event =>
-    {
-        let id = getCurrentEntryID() - 1;
-        switch (id)
-        {
-            case -1: // No entries in current line
-                displayResult.textContent = "";
-            case 0: // A has a value
-            case 2: // B has a value
-                setEntry(id, null);
-                break;
-
-            default: // Operator has a value
-                entries[id] = null;
-        }
-        displayCurrent.textContent = getEntriesDisplayText();
-    });
+    document.getElementById("delete-button").addEventListener("click", () => deleteDigit());
 
     const panelArithmetic = document.querySelector(".panel-arithmetic");
     panelArithmetic.addEventListener("click", event =>
     {
         let index = Array.from(panelArithmetic.children).indexOf(event.target);
-        if(index < 0)
+        if (index < 0)
             return;
-
-        let currentID = getCurrentEntryID();
-        if(currentID < 1)
-        {
-            displayResult.textContent = "enter a number";
-            return;
-        }
-        entries[1] = index;
-        displayCurrent.textContent = getEntriesDisplayText();
+        enterOperator(index);
     });
 
-    document.getElementById("evaluate-button").addEventListener("click", event =>
-    {
-        if (getCurrentEntryID() < 2)
-        {
-            displayResult.textContent = "enter a expression";
-            return;
-        }
-
-        let result = evaluate(entries[0], entries[2], entries[1]);
-        displayResult.textContent = result;
-        displayCurrent.textContent = "";
-        entries = [result, null, null];
-    });
-
+    document.getElementById("evaluate-button").addEventListener("click", () => enterEvaluate());
     document.getElementById("decimal-button").addEventListener("click", () => shouldAddDecimalPoint = true);
 }
 main();
