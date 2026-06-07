5c5a9b2c1d168d2143a55effcb4a36f8dccd14d7
austin <40644715+AustinKr@users.noreply.github.com>
2026-06-06 20:09:52 -0700

    Add functionality for multi-digit numbers

--- javascript.js
+++ javascript.js
@@ -13,14 +13,41 @@ function evaluate(a, b, sign)
     }
     return NaN;
 }
+function isFloatEntryEmpty(float) {
+    return float === null || Number.isNaN(float) || float === Infinity;
+}
+function floatEntryToString(float) {
+    let isEmpty = isFloatEntryEmpty(float);
+    return isEmpty ? "" : float.toString();
+}
+function appendDigitToFloat(float, digitToAppend) {
+    return parseFloat(floatEntryToString(float).concat(digitToAppend));
+}
+function removeDigitFromFloat(float, digitCount = 1)
+{
+    let str = floatEntryToString(float);
+    let newCount = str.length - digitCount;
+    if(newCount <= 0)
+        return null;
+    return parseFloat(str.substring(0, newCount));
+}
 
 let entries = [null, null, null];
+function setEntry(index, digit)
+{
+    if (digit !== null)
+    {
+        entries[index] = appendDigitToFloat(entries[index], digit);
+        return;
+    }
+    entries[index] = removeDigitFromFloat(entries[index]);
+}
 
 function getCurrentEntryID()
 {
     for(let i = 0; i < entries.length; i++)
     {
-        if(entries[i] === null)
+        if (isFloatEntryEmpty(entries[i]))
             return i;
     }
     return entries.length;
@@ -55,16 +82,17 @@ function main()
     const displayCurrent = document.querySelector(".display-current");
 
     const panelNumpad = document.querySelector(".panel-numpad");
-    panelNumpad.addEventListener("click", event => {
+    panelNumpad.addEventListener("click", event =>
+    {
         let index = Array.from(panelNumpad.children).indexOf(event.target);
         if(index < 0)
             return;
 
         let currentID = getCurrentEntryID();
-        if(currentID <= 1)
-            entries[0] = index;
+        if (currentID <= 1)
+            setEntry(0, index);
         else if(currentID > 1)
-            entries[2] = index;
+            setEntry(2, index);
         else
         {
             alert("Please enter a difference type of value");
@@ -72,8 +100,27 @@ function main()
         }
         displayCurrent.textContent = getEntriesDisplayText();
     });
+    document.getElementById("delete-button").addEventListener("click", event =>
+    {
+        let id = getCurrentEntryID() - 1;
+        switch (id)
+        {
+            case -1: // No entries in current line
+                displayResult.textContent = "";
+            case 0: // A has a value
+            case 2: // B has a value
+                setEntry(id, null);
+                break;
+
+            default: // Operator has a value
+                entries[id] = null;
+        }
+        displayCurrent.textContent = getEntriesDisplayText();
+    });
+
     const panelArithmetic = document.querySelector(".panel-arithmetic");
-    panelArithmetic.addEventListener("click", event => {
+    panelArithmetic.addEventListener("click", event =>
+    {
         let index = Array.from(panelArithmetic.children).indexOf(event.target);
         if(index < 0)
             return;
@@ -88,18 +135,23 @@ function main()
         displayCurrent.textContent = getEntriesDisplayText();
     });
 
-    document.getElementById("evaluate-button").addEventListener("click", event => {
+    document.getElementById("evaluate-button").addEventListener("click", event =>
+    {
+        if (getCurrentEntryID() < 2)
+        {
+            alert("Please enter a difference type of value");
+            return;
+        }
+
         let result = evaluate(entries[0], entries[2], entries[1]);
         displayResult.textContent = result;
         displayCurrent.textContent = "";
         entries = [result, null, null];
     });
-    document.getElementById("delete-button").addEventListener("click", event => {
-        let id = Math.max(0, getCurrentEntryID() - 1);
-        entries[id] = null;
-        if(id === 0)
-            displayResult.textContent = "";
-        displayCurrent.textContent = getEntriesDisplayText();
+
+    document.getElementById("decimal-button").addEventListener("click", event =>
+    {
+
     });
 }
 main();
